11bbcf53256af569c3e5913c9276a6ecee9a3e08
kimyoda <rladygks1210@naver.com>
2024-12-11 03:18:00 +0900

    [feat] Layout add

--- loginproject/src/component/layout/Sidebar.styles.ts
+++ loginproject/src/component/layout/Sidebar.styles.ts
@@ -2,8 +2,12 @@ import styled from "styled-components";
 
 export const SidebarContainer = styled.div``;
 
+interface SidebarListItemProps {
+  isActive: boolean;
+}
+
 export const SidebarList = styled.ul``;
 
-export const SidebarListItem = styled.li``;
+export const SidebarListItem = styled.li<SidebarListItemProps>``;
 
 export const LogoutContainer = styled.div``;
