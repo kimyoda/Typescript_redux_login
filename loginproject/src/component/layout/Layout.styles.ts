607af7d4acb906f6761daeabe7ef2f1fdba94d74
kimyoda <rladygks1210@naver.com>
2024-12-13 00:23:02 +0900

    [feat] button, layout, sidebar style add

--- loginproject/src/component/layout/Layout.styles.ts
+++ loginproject/src/component/layout/Layout.styles.ts
@@ -1,7 +1,16 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 interface LayoutContainerProps {
   isLoggedIn: boolean;
 }
 
-export const LayoutContainer = styled.div<LayoutContainerProps>``;
+export const LayoutContainer = styled.div<LayoutContainerProps>`
+  height: 100%;
+  display: flex;
+
+  ${({ isLoggedIn }) =>
+    !isLoggedIn &&
+    css`
+      filter: blur(2px);
+    `}
+`;
